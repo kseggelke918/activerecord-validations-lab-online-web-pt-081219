24d149a6dc9e4236f6bd54f1b3c00e2f91aed5f3
Katie Seggelke <katie.seggelke@gmail.com>
2020-01-08 19:28:04 +0000

    Automatically backed up by Learn

--- app/validators/title_validator.rb
+++ app/validators/title_validator.rb
@@ -1,7 +1,7 @@
 class TitleValidator < ActiveModel::Validator 
-  @@click_bait = [/Won't Believe/, /Secret/, /Guess/, /Top \d+/]
+  CLICK_BAIT = [/Won't Believe/, /Secret/, /Guess/, /Top \d+/]
   def validate(record) 
-    unless record.title.match?(@@click_bait)
+    unless record.title.include?(CLICK_BAIT)
       record.errors[:title] << "isn't clickbait-y enough!"
     end 
   end 
