c30fd576dce79febc28ee02c359e036d0f0b3e64
Katie Seggelke <katie.seggelke@gmail.com>
2020-01-08 19:12:19 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :post_content, length: { minimum: 250 }
-  validates :post_summary, length: { maximum: 250 }
-  validates :post_category, inclusion: { in: %w(fiction non-fiction), message: "%{value} is not a valid category"}
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(fiction non-fiction), message: "%{value} is not a valid category"}
 end
